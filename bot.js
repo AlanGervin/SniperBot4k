048ec00e562f153548de3023665c65dcd4ee8f76
Alan Gervin <alan.gervin@gmail.com>
2020-08-26 23:28:16 -0700

    did code refactor to make it look clearner

--- bot.js
+++ bot.js
@@ -2,90 +2,87 @@ const tmi = require('tmi.js');
 const process = require('process');
 var client;
 var client2;
-var num = 5;
+var opts;
+var timer1;
 
-getYourLobby = () => {
-      
-      // Define configuration options
-      var opts2 = {
-        identity: {
-          username: <replace with the name of the bot in quotes and remove the brakets>,
-          password: <replace with the oauth code you get from twitch surrounded by quotes and remove the brackets>
-        },
-        channels: [
-          process.argv[2]
-        ]
-      };
-      // Create client with our options
-      client2 = new tmi.client(opts2);
-      //register handlers
-      client2.on('message', onMessageHandler);
-      client2.on('connected', onConnectedHandler);
-      client2.connect();
+
+getYourLobby = () => {      
+  // Define configuration options
+  var opts = {
+    identity: {
+      //replace the <> with the bot name in quotes. example: "BotName123"
+      username: <>,
+      //reaplce the next line <> with the oauth code you get get from twitch surrounded by quotes. example "oauth1as2fd1asdf54f"
+      password: <>
+    },
+    channels: [
+      process.argv[2]
+    ]
+  };
+  // Create client with our options
+  client2 = new tmi.client(opts);
+  //register handlers
+  client2.on('message', onMessageHandler);
+  client2.on('connected', onConnectedHandler);
+  client2.connect();
 };
 
 getOpponentLobby = () => { 
-      // Define configuration options
-      var opts = {
-        identity: { 
-          username: <replace with the name of the bot in quotes and remove the brakets>,
-          password: <replace with the oauth code you get from twitch surrounded by quotes and remove the brackets>
-        },
-        channels: [
-          process.argv[3]
-        ]
-      };
-      // Create client with our options
-      client = new tmi.client(opts);
-      //register handlers
-      client.on('message', onMessageHandler);
-      client.on('connected', onConnectedHandler);
-      client.connect();
+  // Define configuration options
+  opts = {
+    identity: { 
+      //replace the <> with the bot name in quotes. example: "BotName123"
+      username: <>,
+      //replace the next line <> with the oauth code you get get from twitch surrounded by quotes. example "oauth1as2fd1asdf54f"
+      password: <>
+    },
+    channels: [
+      process.argv[3]
+    ]
+  };
+  // Create client with our options
+  client = new tmi.client(opts);
+  //register handlers
+  client.on('message', onMessageHandler);
+  client.on('connected', onConnectedHandler);
+  client.connect();
 };
-  
 
 getOpponentLobby();
-
 getYourLobby();
-
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
+  var num;
   if (self) { return; } // Ignore messages from the bot
-
+  
   // Remove whitespace from chat message
   const commandName = msg.trim();
+  
+  //Check if anyone in chat said 5 or 5 Seconds! or Ready
   if (commandName === '5' || commandName === "5 seconds!") {
-    var timer1 = setInterval(function(){
-      if(num > 0) {
-        client2.say(process.argv[2], num.toString());
-        console.log(num)
-        num--;
-      }
-      if (num == 0) {
-          clearInterval(timer1);
-      }
-    },1000);
     num = 5;
+    timer(num,client2);
+    
     return;
   } else if (commandName === "Ready") {
-    num=4;
-    var timer1 = setInterval(function(){
+    num = 4;
+    timer(num,client2); 
+  }
+}
+
+function timer(number,client) {
+  var num = number;
+  timer1 = setInterval(function(){
       if(num > 0) {
-        client2.say(process.argv[2], num.toString());
+        client.say(process.argv[2], num.toString());
         console.log(num)
         num--;
       }
       if (num == 0) {
-          clearInterval(timer1);
-      }
-    },1000);
-    num = 5;
-    return;
-  } else {
-    console.log("unknown command")
-  }
-  
+        clearInterval(timer1);
+    }
+  },1000);
 }
 
 
